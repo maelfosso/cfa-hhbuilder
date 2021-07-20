01438eb2c3cd40ab3a007d952b19042aa3d1a8b0
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-07-20 19:04:29 +0100

    added event and eventAggregator

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // your code goes here ...
+let household = [];
+
+class Event {
+  constructor(name) {
+    this.name = name;
+    this.handlers = [];
+  }
+
+  addHandler(handler) {
+    this.handlers.push(handler);
+  }
+
+  removeHandler(handler) {
+    for(let i=0; i < this.handlers.length; i++) {
+      if (this.handlers[i] == handler) {
+        this.handlers.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  fire(eventArgs) {
+    this.handlers.forEach((handler) => {
+      handler(eventArgs);
+    });
+  }
+}
+
+const eventAggregator = (() => {
+  let events = [];
+
+  const getEvent = (eventName) => {
+    let event = events.filter((event) => {
+      return event.name == eventName;
+    })[0];
+
+    if (!event) {
+      event = new Event(eventName);
+      events.push(event);
+    }
+
+    return event;
+  }
+
+  const publish = (eventName, eventArgs) => {
+    let event = getEvent(eventName);
+
+    event.fire(eventArgs);
+  }
+
+  const subscribe = (eventName, handler) => {
+    let event = getEvent(eventName);
+    event.addHandler(handler);
+  }
+
+  return {
+    publish,
+    subscribe
+  };
+})();
+
+
+class Person {
+  constructor(age, relationship, smoker) {
+    this.age = age;
+    this.relationship = relationship;
+    this.smoker = smoker;
+  }
+}
