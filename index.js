ccef141ce2f35145690014bb1f9054c591ebcc7b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-07-20 19:16:12 +0100

    added person form module

--- index.js
+++ index.js
@@ -68,3 +68,31 @@ class Person {
     this.smoker = smoker;
   }
 }
+
+const addPersonForm = (() => {
+  const listener = () => {
+    document.querySelector('form').addEventListener('submit', function(e) {
+      e.preventDefault();
+      console.log('Form submission ', e);
+    });
+
+    document.querySelector('button.add').addEventListener('click', function(e) {
+      e.preventDefault();
+      console.log('Button add ', e);
+    })
+  }
+
+  const init = () => {
+    listener();
+  }
+
+  return {
+    init,
+  }
+})();
+
+const init = () => {
+  addPersonForm.init();
+}
+
+init();
